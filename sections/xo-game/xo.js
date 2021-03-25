52d82e0bde3ddab6dac1f79dc60fb3946ab0b367
amberTrance <mind.edifice@gmail.com>
2021-03-25 17:10:34 +0200

    Added non-empty-square click rule, and css cursor:pointer

--- sections/xo-game/xo.js
+++ sections/xo-game/xo.js
@@ -2,18 +2,30 @@ let squares = document.querySelectorAll('.xo-square')
 
 squares.forEach(square => {
     square.addEventListener('click', () => {
-        // when the user clicks on a square, add 'o'      
-        square.classList.add('o-sign')
-        
-        // then get a new array with the filtered 'o's
-        let leftSquares = [...squares].filter(square => (!square.classList.contains('o-sign')) & (!square.classList.contains('x-sign')))
-    
-        // in one of the empty squares, add an x
-        leftSquares[0].classList.add('x-sign')
+        // If the user clicks on a non-empty square, alert error
+        if (
+            square.classList.contains('o-sign') 
+            ||
+            square.classList.contains('x-sign')
+        ) {
 
-        //if there are less than 6 squares left, start tracking for 3 in a row:
-        if ((leftSquares.length - 1) < 6) {
+            alert('Choose an empty square, please!');
+
+        } else {
+
+            // when the user clicks on a square, add 'o'      
+            square.classList.add('o-sign')
             
+            // then filter out the non-empty squares
+            let leftSquares = [...squares].filter(square => (!square.classList.contains('o-sign')) & (!square.classList.contains('x-sign')))
+        
+            // in one of the empty squares, add an x
+            leftSquares[0].classList.add('x-sign')
+
+            //if there are less than 6 squares left, start tracking for 3 in a row:
+            if ((leftSquares.length - 1) < 6) {
+                
+            }
         }
     })
 })
