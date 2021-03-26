b1787770cefaa9673364bcc0a82dd6e71eceb59c
amberTrance <mind.edifice@gmail.com>
2021-03-26 18:29:43 +0200

    added win strikes

--- sections/xo-game/xo.js
+++ sections/xo-game/xo.js
@@ -1,30 +1,136 @@
-let squares = document.querySelectorAll('.xo-square')
+const squares = document.querySelectorAll('.xo-square')
 
+// When clicking a square:
 squares.forEach(square => {
     square.addEventListener('click', () => {
-        // If the user clicks on a non-empty square, alert error
+
+        // If the user clicks on a non-empty square, do nothing
         if (
             square.classList.contains('o-sign') 
             ||
             square.classList.contains('x-sign')
         ) {
-
-            alert('Choose an empty square, please!');
-
+            return;
         } else {
 
-            // when the user clicks on a square, add 'o'      
-            square.classList.add('o-sign')
-            
-            // then filter out the non-empty squares
-            let leftSquares = [...squares].filter(square => (!square.classList.contains('o-sign')) & (!square.classList.contains('x-sign')))
-        
-            // in one of the empty squares, add an x
-            leftSquares[0].classList.add('x-sign')
+            // when the user clicks on an empty square, add 'o'      
+            square.classList.add('o-sign') 
 
-            //if there are less than 6 squares left, start tracking for 3 in a row:
-            if ((leftSquares.length - 1) < 6) {
+
+            // Grab the leftover squares
+            let leftSquares = [...squares].filter(square => (!square.classList.contains('o-sign')) 
+            & (!square.classList.contains('x-sign')))
+
+
+            // Pick a random square and mark it with x
+            const random = Math.floor(Math.random() * leftSquares.length);
+            leftSquares[random].classList.add('x-sign')
+            let x = leftSquares[random].id
+
+
+            // Check for winning
+            if ( leftSquares.length < 5 ) {
                 
+                /* If the first row of 3 all have o-sign or x-sign, 
+                add a class of win-line-a which crosses the first row*/
+                if ( 
+                    squares[0].className === "xo-square o-sign" &
+                    squares[1].className === "xo-square o-sign" &
+                    squares[2].className === "xo-square o-sign"
+                    ||
+                    squares[0].className === "xo-square x-sign" &
+                    squares[1].className === "xo-square x-sign" &
+                    squares[2].className === "xo-square x-sign"  
+                ) {
+
+                    document.getElementById('xo-container').classList.add('win-line-a')
+
+                } else if (
+                    squares[3].className === "xo-square o-sign" &
+                    squares[4].className === "xo-square o-sign" &
+                    squares[5].className === "xo-square o-sign"
+                    ||
+                    squares[3].className === "xo-square x-sign" &
+                    squares[4].className === "xo-square x-sign" &
+                    squares[5].className === "xo-square x-sign"
+                ) {
+                    
+                    document.getElementById('xo-container').classList.add('win-line-b')
+                    
+                } else if (
+                    squares[6].className === "xo-square o-sign" &
+                    squares[7].className === "xo-square o-sign" &
+                    squares[8].className === "xo-square o-sign"
+                    ||
+                    squares[6].className === "xo-square x-sign" &
+                    squares[7].className === "xo-square x-sign" &
+                    squares[8].className === "xo-square x-sign"                    
+                ) {
+
+                    document.getElementById('xo-container').classList.add('win-line-c')
+
+                } else if (
+                    squares[0].className === "xo-square o-sign" &
+                    squares[3].className === "xo-square o-sign" &
+                    squares[6].className === "xo-square o-sign"
+                    ||
+                    squares[0].className === "xo-square x-sign" &
+                    squares[3].className === "xo-square x-sign" &
+                    squares[6].className === "xo-square x-sign"
+                ) {
+
+                    document.getElementById('xo-container').classList.add('win-line-1')
+                    
+                } else if (
+                    squares[1].className === "xo-square o-sign" &
+                    squares[4].className === "xo-square o-sign" &
+                    squares[7].className === "xo-square o-sign"
+                    ||
+                    squares[1].className === "xo-square x-sign" &
+                    squares[4].className === "xo-square x-sign" &
+                    squares[7].className === "xo-square x-sign"
+                ) {
+
+                    document.getElementById('xo-container').classList.add('win-line-2')
+
+                } else if (
+                    squares[2].className === "xo-square o-sign" &
+                    squares[5].className === "xo-square o-sign" &
+                    squares[8].className === "xo-square o-sign"
+                    ||
+                    squares[2].className === "xo-square x-sign" &
+                    squares[5].className === "xo-square x-sign" &
+                    squares[8].className === "xo-square x-sign"
+                ) {
+
+                    document.getElementById('xo-container').classList.add('win-line-3')
+
+                } else if (
+                    squares[0].className === "xo-square o-sign" &
+                    squares[4].className === "xo-square o-sign" &
+                    squares[8].className === "xo-square o-sign"
+                    ||
+                    squares[0].className === "xo-square x-sign" &
+                    squares[4].className === "xo-square x-sign" &
+                    squares[8].className === "xo-square x-sign"
+                ) {
+
+                    document.getElementById('xo-container').classList.add('win-diagon-1')
+
+                } else if (
+                    squares[2].className === "xo-square o-sign" &
+                    squares[4].className === "xo-square o-sign" &
+                    squares[6].className === "xo-square o-sign"
+                    ||
+                    squares[2].className === "xo-square x-sign" &
+                    squares[4].className === "xo-square x-sign" &
+                    squares[6].className === "xo-square x-sign"
+                ) {
+
+                    document.getElementById('xo-container').classList.add('win-diagon-2')
+
+                }
+
             }
         }
     })
